75efac53e80ae165c62f6bfbb95ac367d0dae72e
Shaun <shaun@thenetninja.co.uk>
2021-01-04 16:57:35 +0000

    lesson-4

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Ninja Listing</a>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/ninjas/"><a>Ninja Listing</a></Link>
     </nav>
 );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <Link href="/ninjas/">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
