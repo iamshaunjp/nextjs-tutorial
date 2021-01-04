5159f0cc53c73ed551cd356d73a0cd52ece4a4c6
Shaun <shaun@thenetninja.co.uk>
2021-01-04 17:01:30 +0000

    lesson-5

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <Link href="/ninjas/">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
--- pages/ninjas/test.js
+++
@@ -1,9 +0,0 @@
-const Test = () => {
-  return (
-    <div>
-      <h1>Test page</h1>
-    </div>
-  );
-}
- 
-export default Test;
