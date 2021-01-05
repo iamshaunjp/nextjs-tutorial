a1c7d69d8ca8e5764c41dcc3c1d1170da2326b82
Shaun <shaun@thenetninja.co.uk>
2021-01-05 12:03:44 +0000

    lesson-13

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -15,10 +15,23 @@ export const getStaticPaths = async () => {
   }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
+  }
+}
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p>
     </div>
   );
 }
