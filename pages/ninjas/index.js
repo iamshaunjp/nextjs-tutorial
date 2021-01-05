6cbb2790738620bbb62c0640e19973f0990f73b4
Shaun <shaun@thenetninja.co.uk>
2021-01-05 11:00:54 +0000

    lesson-10

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,27 @@
-const Ninjas = () => {
+import styles from '../../styles/Jobs.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  console.log(ninjas)
+
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
