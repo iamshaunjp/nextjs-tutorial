19af9488e656d20e84f8b8479981a7dea563857a
Shaun <shaun@thenetninja.co.uk>
2021-01-05 11:15:02 +0000

    lesson-11

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div>
+      <h1>Details Page</h1>
+    </div>
+  );
+}
+
+export default Details;
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
-import styles from '../../styles/Jobs.module.css'
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -16,11 +17,11 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ ninja.name }</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
