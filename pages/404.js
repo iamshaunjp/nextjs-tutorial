e8e53daea39457509f2d0dd9162d8e3551f42c02
Shaun <shaun@thenetninja.co.uk>
2021-01-05 09:05:54 +0000

    lesson-8

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,23 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+      // router.go(1)
+      router.push('/')
+    }, 3000)
+  }, [])
+
   return (
     <div className="not-found">
       <h1>Ooops...</h1>
       <h2>That page cannot be found :(</h2>
-      <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+      <p>Going back to the <Link href="/"><a>Homepage</a></Link> is 3 seconds...</p>
     </div>
   );
 }
