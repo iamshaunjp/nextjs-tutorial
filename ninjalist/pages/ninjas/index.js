17cdbb0eb35bd5a8f7b50ab9e449019deae7b8fa
eddiepmonark <53535274+eddiepmonark@users.noreply.github.com>
2021-02-22 15:38:55 -0800

    Fetching Data (getStaticProps)

--- ninjalist/pages/ninjas/index.js
+++ ninjalist/pages/ninjas/index.js
@@ -1,9 +1,32 @@
-const Ninja = () => {
+import styles from '../../styles/Ninjas.module.css'
+// import styles from '../styles/Home.module.css'
+
+export const getStaticProps = async () => {
+    const res = await fetch('http://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: {
+            ninjas: data
+        }
+    }
+}
+
+
+
+const Ninjas = ({ ninjas }) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <div key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ ninja.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
  
-export default Ninja;
+export default Ninjas;
