4630a4eaa9762a36d3614a46ab33d7d42bb0e0b5
eddiepmonark <53535274+eddiepmonark@users.noreply.github.com>
2021-02-22 16:20:21 -0800

    links and Dynamic Routes (part 2 - getStaticPaths)

--- ninjalist/pages/ninjas/index.js
+++ ninjalist/pages/ninjas/index.js
@@ -1,32 +1,30 @@
 import styles from '../../styles/Ninjas.module.css'
-// import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
-    const res = await fetch('http://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
 
-    return {
-        props: {
-            ninjas: data
-        }
-    }
+  return {
+    props: { ninjas: data }
+  }
 }
 
-
-
 const Ninjas = ({ ninjas }) => {
-    return ( 
-        <div>
-            <h1>All Ninjas</h1>
-            {ninjas.map(ninja => (
-                <div key={ninja.id}>
-                    <a className={styles.single}>
-                        <h3>{ ninja.name }</h3>
-                    </a>
-                </div>
-            ))}
-        </div>
-     );
+  // console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
 }
  
 export default Ninjas;
